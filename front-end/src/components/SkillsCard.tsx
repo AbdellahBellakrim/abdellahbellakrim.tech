e0f180aab22cff5aaf345927e6e025c6a8c87036
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-12-18 16:14:22 +0100

    updating skills sections and learning stuff to add dynamic data from firebase to this section

--- front-end/src/components/SkillsCard.tsx
+++ front-end/src/components/SkillsCard.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Divider, Image } from "@nextui-org/react";
 
 export default function SkillsCard() {
   return (
-    <Card className="max-w-[400px] bg-[#292C36] shadow-none text-[#B0ADAD] h-[190px]">
-      <CardHeader className="flex gap-3 h-[60%]">
+    <Card className="max-w-[400px] bg-[#292C36] shadow-none text-[#B0ADAD] h-fit">
+      <CardHeader className="flex gap-3 h-[40%]">
         <div className="w-[42px] h-[42px] rounded-md bg-[#38405A] flex justify-center items-center">
           <Image
             alt="nextui logo"
             height={30}
             width={30}
-            radius="sm"
             src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
           />
         </div>
