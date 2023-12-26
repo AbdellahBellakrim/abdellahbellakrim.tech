351280b2d46137f44e2f34b0b24eac5ca1e6afae
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-12-26 22:39:08 +0100

    reduce animation time

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -21,7 +21,7 @@ const HOC = (WrappedComponent: any) => {
         try {
           setSkill(skills);
           setProject(projects);
-          setTimeout(() => setLoadingData(false), 5000);
+          setTimeout(() => setLoadingData(false), 2000);
         } catch (error) {
           console.clear();
         }
